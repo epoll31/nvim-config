c39c854305a3dc7cdb263b16b72dfe98ffd66308
ethan <epollack31@gmail.com>
2025-03-18 20:48:54 -0700

    basic script picker added

--- lua/plugins/pm.lua
+++ lua/plugins/pm.lua
@@ -1,62 +1,3 @@
-local function gen_scripts_file_paths()
-	local session_projects = require("neovim-project.utils.history").session_projects
-
-	local relative_script_path = "/.nvim/scripts.json"
-	local script_paths = {}
-	for i, path in pairs(session_projects) do
-		script_paths[i] = path .. relative_script_path
-		-- vim.api.nvim_out_write(script_paths[i] .. "/n")
-	end
-
-	return script_paths
-end
-
---@type table
-local function parse_scripts_file(path)
-	local ok, json_string = pcall(function()
-		local _, err = io.open(path, "r")
-		if err ~= nil then
-			return ""
-		end
-		local json = io.read("*all")
-		-- io.close(file)
-		return json
-	end)
-
-	if not ok then
-		vim.api.nvim_err_writeln("Failed to read file: " .. json_string)
-		return {}
-	end
-	vim.api.nvim_err_writeln("json_string: " .. json_string)
-	local ok, json_data = pcall(vim.json.decode, json_string)
-
-	if not ok then
-		vim.api.nvim_err_writeln("Failed to parse JSON: " .. json_data)
-		return {}
-	end
-
-	return json_data
-end
-
-local function run_default_script()
-	local in_sesson = require("neovim-project.project").in_session()
-	if in_sesson == false then
-		vim.notify("session not started")
-	end
-
-	local inspect = require("vim.inspect")
-	local scripts_file_paths = gen_scripts_file_paths()
-
-	for _, path in ipairs(scripts_file_paths) do
-		local scripts = parse_scripts_file(path)
-		vim.notify(inspect(scripts))
-	end
-
-	-- vim.notify(inspect(scripts_file_paths))
-end
-
--- Map a keybinding to run the script
-
 return {
 	{
 		"coffebar/neovim-project",
@@ -77,8 +18,6 @@ return {
 				},
 				picker = { type = "telescope" },
 			})
-
-			-- vim.keymap.set("n", "<leader>r<CR>", run_default_script, { noremap = true, silent = true })
 		end,
 	},
 
@@ -91,50 +30,86 @@ return {
 			})
 		end,
 	},
+
 	{
-		"dimaportenko/project-cli-commands.nvim",
-
-		dependencies = {
-			"akinsho/toggleterm.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-
-		-- optional keymap config
+		-- "diniamo/run.nvim",
+		dir = "~/projects/run.nvim/",
+		lazy = false,
 		config = function()
-			-- local OpenActions = require('project_cli_commands.open_actions')
-			-- local RunActions = require('project_cli_commands.actions')
-			--
-			-- local config = {
-			--   -- Key mappings bound inside the telescope window
-			--   running_telescope_mapping = {
-			--     ['<C-c>'] = RunActions.exit_terminal,
-			--     ['<C-f>'] = RunActions.open_float,
-			--     ['<C-v>'] = RunActions.open_vertical,
-			--     ['<C-h>'] = RunActions.open_horizontal,
-			--   },
-			--   open_telescope_mapping = {
-			--     { mode = 'i', key = '<CR>',  action = OpenActions.execute_script_vertical },
-			--     { mode = 'n', key = '<CR>',  action = OpenActions.execute_script_vertical },
-			--     { mode = 'i', key = '<C-h>', action = OpenActions.execute_script },
-			--     { mode = 'n', key = '<C-h>', action = OpenActions.execute_script },
-			--     { mode = 'i', key = '<C-i>', action = OpenActions.execute_script_with_input },
-			--     { mode = 'n', key = '<C-i>', action = OpenActions.execute_script_with_input },
-			--     { mode = 'i', key = '<C-c>', action = OpenActions.copy_command_clipboard },
-			--     { mode = 'n', key = '<C-c>', action = OpenActions.copy_command_clipboard },
-			--     { mode = 'i', key = '<C-f>', action = OpenActions.execute_script_float },
-			--     { mode = 'n', key = '<C-f>', action = OpenActions.execute_script_float },
-			--     { mode = 'i', key = '<C-v>', action = OpenActions.execute_script_vertical },
-			--     { mode = 'n', key = '<C-v>', action = OpenActions.execute_script_vertical },
-			--   }
-			-- }
+			local run = require("run")
 
-			--
-			-- vim.api.nvim_set_keymap("n", "<leader>p", ":Telescope project_cli_commands open<cr>",
-			-- { noremap = true, silent = true })
-			-- vim.api.nvim_set_keymap("n", "<leader>;", ":Telescope project_cli_commands running<cr>",
-			-- { noremap = true, silent = true })
-
-			-- require('project_cli_commands').setup(config)
+			-- Runs the cached command
+			vim.keymap.set("n", "<leader>ri", require("run").run)
+			-- Prompts for a command, and overrides the cached one with it
+			vim.keymap.set("n", "<leader>ro", function()
+				require("run").run(nil, true)
+			end)
+			-- Prompts for a command to run, without overriding
+			vim.keymap.set("n", "<leader>rc", require("run").run_prompt)
 		end,
 	},
+
+	{
+		dir = "~/projects/script_runner.nvim",
+	},
+
+	-- {
+	-- 	"CRAG666/code_runner.nvim",
+	-- 	config = function()
+	-- 		vim.keymap.set("n", "<leader>rr", ":RunCode<CR>", { noremap = true, silent = false })
+	-- 		vim.keymap.set("n", "<leader>rf", ":RunFile<CR>", { noremap = true, silent = false })
+	-- 		vim.keymap.set("n", "<leader>rft", ":RunFile tab<CR>", { noremap = true, silent = false })
+	-- 		vim.keymap.set("n", "<leader>rp", ":RunProject<CR>", { noremap = true, silent = false })
+	-- 		vim.keymap.set("n", "<leader>rc", ":RunClose<CR>", { noremap = true, silent = false })
+	-- 		vim.keymap.set("n", "<leader>crf", ":CRFiletype<CR>", { noremap = true, silent = false })
+	-- 		vim.keymap.set("n", "<leader>crp", ":CRProjects<CR>", { noremap = true, silent = false })
+	-- 	end,
+	-- },
+
+	-- {
+	-- 	"dimaportenko/project-cli-commands.nvim",
+	--
+	-- 	dependencies = {
+	-- 		"akinsho/toggleterm.nvim",
+	-- 		"nvim-telescope/telescope.nvim",
+	-- 	},
+	--
+	-- 	-- optional keymap config
+	-- 	config = function()
+	-- 		-- local OpenActions = require('project_cli_commands.open_actions')
+	-- 		-- local RunActions = require('project_cli_commands.actions')
+	-- 		--
+	-- 		-- local config = {
+	-- 		--   -- Key mappings bound inside the telescope window
+	-- 		--   running_telescope_mapping = {
+	-- 		--     ['<C-c>'] = RunActions.exit_terminal,
+	-- 		--     ['<C-f>'] = RunActions.open_float,
+	-- 		--     ['<C-v>'] = RunActions.open_vertical,
+	-- 		--     ['<C-h>'] = RunActions.open_horizontal,
+	-- 		--   },
+	-- 		--   open_telescope_mapping = {
+	-- 		--     { mode = 'i', key = '<CR>',  action = OpenActions.execute_script_vertical },
+	-- 		--     { mode = 'n', key = '<CR>',  action = OpenActions.execute_script_vertical },
+	-- 		--     { mode = 'i', key = '<C-h>', action = OpenActions.execute_script },
+	-- 		--     { mode = 'n', key = '<C-h>', action = OpenActions.execute_script },
+	-- 		--     { mode = 'i', key = '<C-i>', action = OpenActions.execute_script_with_input },
+	-- 		--     { mode = 'n', key = '<C-i>', action = OpenActions.execute_script_with_input },
+	-- 		--     { mode = 'i', key = '<C-c>', action = OpenActions.copy_command_clipboard },
+	-- 		--     { mode = 'n', key = '<C-c>', action = OpenActions.copy_command_clipboard },
+	-- 		--     { mode = 'i', key = '<C-f>', action = OpenActions.execute_script_float },
+	-- 		--     { mode = 'n', key = '<C-f>', action = OpenActions.execute_script_float },
+	-- 		--     { mode = 'i', key = '<C-v>', action = OpenActions.execute_script_vertical },
+	-- 		--     { mode = 'n', key = '<C-v>', action = OpenActions.execute_script_vertical },
+	-- 		--   }
+	-- 		-- }
+	--
+	-- 		--
+	-- 		-- vim.api.nvim_set_keymap("n", "<leader>p", ":Telescope project_cli_commands open<cr>",
+	-- 		-- { noremap = true, silent = true })
+	-- 		-- vim.api.nvim_set_keymap("n", "<leader>;", ":Telescope project_cli_commands running<cr>",
+	-- 		-- { noremap = true, silent = true })
+	--
+	-- 		-- require('project_cli_commands').setup(config)
+	-- 	end,
+	-- },
 }
